1eb4e52abda98c6220104fd1a60dbc1203b96a2c
Jake Leventhal <jakeleventhal@me.com>
2017-11-10 18:39:18 -0800

    registration works

---
+++ Coliseum.sql
@@ -0,0 +1,21 @@
+
+DROP DATABASE IF EXISTS ColiseumDatabase;
+CREATE DATABASE ColiseumDatabase;
+USE ColiseumDatabase;
+
+CREATE TABLE Users (
+	username VARCHAR(25) PRIMARY KEY NOT NULL,
+	pass VARCHAR(25) NOT NULL,
+	highScore INT(25)
+);
+
+CREATE TABLE HighScores (
+	username VARCHAR(25) PRIMARY KEY NOT NULL,
+	highScore int(25) NOT NULL
+);
+
+INSERT INTO Users (username, pass, highScore)
+	VALUES ('testUser', 'testPass');
+
+INSERT INTO HighScores (username, highScore)
+	VALUES ('testUser', 69420);
